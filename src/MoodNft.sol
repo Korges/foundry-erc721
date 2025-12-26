bc20655fba225e74d21cb96daf9adacbaebc1ff8
korges <korgesik@gmail.com>
2025-12-26 14:42:39 +0100

    MoodNft smartcontract

---
+++ src/MoodNft.sol
@@ -0,0 +1,72 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
+
+contract MoodNft is ERC721 {
+    error ERC721Metadata__URI_QueryFor_NonExistentToken();
+    enum NFTState {
+        HAPPY,
+        SAD
+    }
+    uint256 private s_tokenCounter;
+    string private s_sadSvgUri;
+    string private s_happySvgUri;
+
+    mapping(uint256 => NFTState) private s_tokenIdToState;
+
+    constructor(
+        string memory sadSvgUri,
+        string memory happySvgUri
+    ) ERC721("Mood NFT", "MNFT") {
+        s_tokenCounter = 0;
+        s_sadSvgUri = sadSvgUri;
+        s_happySvgUri = happySvgUri;
+    }
+
+    function mintNft() public {
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter++;
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "data:application/json;base64,";
+    }
+
+    function tokenURI(
+        uint256 tokenId
+    ) public view virtual override returns (string memory) {
+        if (ownerOf(tokenId) == address(0)) {
+            revert ERC721Metadata__URI_QueryFor_NonExistentToken();
+        }
+        string memory imageURI = s_happySvgUri;
+
+        if (s_tokenIdToState[tokenId] == NFTState.SAD) {
+            imageURI = s_sadSvgUri;
+        }
+        return
+            string(
+                abi.encodePacked(
+                    _baseURI(),
+                    Base64.encode(
+                        bytes( // bytes casting actually unnecessary as 'abi.encodePacked()' returns a bytes
+                            abi.encodePacked(
+                                '{"name":"',
+                                name(), // You can add whatever name here
+                                '", "description":"An NFT that reflects the mood of the owner, 100% on Chain!", ',
+                                '"attributes": [{"trait_type": "moodiness", "value": 100}], "image":"',
+                                imageURI,
+                                '"}'
+                            )
+                        )
+                    )
+                )
+            );
+    }
+
+    function getTokenCounter() public view returns (uint256) {
+        return s_tokenCounter;
+    }
+}
---
+++ test/MoodNftTest.t.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {DeployMoodNft} from "../script/DeployMoodNft.s.sol";
+import {MoodNft} from "../src/MoodNft.sol";
+import {Test, console} from "forge-std/Test.sol";
+import {Vm} from "forge-std/Vm.sol";
+import {MintBasicNft} from "../script/Interactions.s.sol";
+import {ZkSyncChainChecker} from "lib/foundry-devops/src/ZkSyncChainChecker.sol";
+import {FoundryZkSyncChecker} from "lib/foundry-devops/src/FoundryZkSyncChecker.sol";
+
+contract MoodNftTest is Test, ZkSyncChainChecker, FoundryZkSyncChecker {
+    string constant NFT_NAME = "Mood NFT";
+    string constant NFT_SYMBOL = "MNFT";
+    MoodNft public moodNft;
+    DeployMoodNft public deployer;
+    address public deployerAddress;
+
+    string public constant HAPPY_MOOD_URI =
+        "data:application/json;base64,eyJuYW1lIjoiTW9vZCBORlQiLCAiZGVzY3JpcHRpb24iOiJBbiBORlQgdGhhdCByZWZsZWN0cyB0aGUgbW9vZCBvZiB0aGUgb3duZXIsIDEwMCUgb24gQ2hhaW4hIiwgImF0dHJpYnV0ZXMiOiBbeyJ0cmFpdF90eXBlIjogIm1vb2RpbmVzcyIsICJ2YWx1ZSI6IDEwMH1dLCAiaW1hZ2UiOiJkYXRhOmltYWdlL3N2Zyt4bWw7YmFzZTY0LFBITjJaeUIyYVdWM1FtOTRQU0l3SURBZ01qQXdJREl3TUNJZ2QybGtkR2c5SWpRd01DSWdJR2hsYVdkb2REMGlOREF3SWlCNGJXeHVjejBpYUhSMGNEb3ZMM2QzZHk1M015NXZjbWN2TWpBd01DOXpkbWNpUGdvZ0lEeGphWEpqYkdVZ1kzZzlJakV3TUNJZ1kzazlJakV3TUNJZ1ptbHNiRDBpZVdWc2JHOTNJaUJ5UFNJM09DSWdjM1J5YjJ0bFBTSmliR0ZqYXlJZ2MzUnliMnRsTFhkcFpIUm9QU0l6SWk4K0NpQWdQR2NnWTJ4aGMzTTlJbVY1WlhNaVBnb2dJQ0FnUEdOcGNtTnNaU0JqZUQwaU56QWlJR041UFNJNE1pSWdjajBpTVRJaUx6NEtJQ0FnSUR4amFYSmpiR1VnWTNnOUlqRXlOeUlnWTNrOUlqZ3lJaUJ5UFNJeE1pSXZQZ29nSUR3dlp6NEtJQ0E4Y0dGMGFDQmtQU0p0TVRNMkxqZ3hJREV4Tmk0MU0yTXVOamtnTWpZdU1UY3ROalF1TVRFZ05ESXRPREV1TlRJdExqY3pJaUJ6ZEhsc1pUMGlabWxzYkRwdWIyNWxPeUJ6ZEhKdmEyVTZJR0pzWVdOck95QnpkSEp2YTJVdGQybGtkR2c2SURNN0lpOCtDand2YzNablBnPT0ifQ==";
+
+    address public constant USER = address(1);
+
+    function setUp() public {
+        deployer = new DeployMoodNft();
+        if (!isZkSyncChain()) {
+            moodNft = deployer.run();
+        } else {
+            string memory sadSvg = vm.readFile("./img/sad.svg");
+            string memory happySvg = vm.readFile("./img/happy.svg");
+            moodNft = new MoodNft(deployer.svgToImageURI(sadSvg), deployer.svgToImageURI(happySvg));
+        }
+    }
+
+    function testInitializedCorrectly() public view {
+        assert(keccak256(abi.encodePacked(moodNft.name())) == keccak256(abi.encodePacked((NFT_NAME))));
+        assert(keccak256(abi.encodePacked(moodNft.symbol())) == keccak256(abi.encodePacked((NFT_SYMBOL))));
+    }
+
+    function testCanMintAndHaveABalance() public {
+        vm.prank(USER);
+        moodNft.mintNft();
+
+        assert(moodNft.balanceOf(USER) == 1);
+    }
+
+    function testTokenURIDefaultIsCorrectlySet() public {
+        vm.prank(USER);
+        moodNft.mintNft();
+
+        assert(keccak256(abi.encodePacked(moodNft.tokenURI(0))) == keccak256(abi.encodePacked(HAPPY_MOOD_URI)));
+    }
+}
